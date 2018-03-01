86b3085afe4a08aabaf99d34442e8391ea827956
Pawarisson <kan.pawarisson@gmail.com>
2018-03-01 18:25:43 +0700

    HW 8.2 done

--- programing/machine-learning-ex8/ex8/selectThreshold.m
+++ programing/machine-learning-ex8/ex8/selectThreshold.m
@@ -22,19 +22,15 @@ for epsilon = min(pval):stepsize:max(pval)
     %               
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
+    predict = (pval < epsilon);
+    tp = sum(predict==yval & predict == 1);
+    fp = sum(predict!=yval & predict == 1);
+    fn = sum(predict!=yval & predict == 0);
 
+    prec = tp/(tp+fp);
+    rec = tp/(tp+fn);
 
-
-
-
-
-
-
-
-
-
-
-
+    F1 = 2*prec*rec/(prec+rec);
     % =============================================================
 
     if F1 > bestF1
